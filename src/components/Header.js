42f05216731e6c469b08564d4fcb35af709b1629
Andrew Hansbrough <hansbroughku@gmail.com>
2020-07-29 15:24:21 -0500

    Added handleinputchange to searchform in header

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,20 +3,20 @@ import SearchForm from "./SearchForm.js";
 
 const styles = {
   headerStyle: {
-    background: "skyblue",
+    background: "transparent",
     textAlign: "center",
     alignSelf: "stretch",
     paddingBottom: 10
   }
 };
 
-function Header() {
+function Header({ handleInputChange }) {
   return (
     <div className="container">
       <div className="jumbotron jumbotron-fluid" style={styles.headerStyle}>
         <div className="container">
           <h1 className="display-4">Employee Directory</h1>
-          <SearchForm />
+          <SearchForm handleInputChange={handleInputChange} />
         </div>
       </div>
     </div>
