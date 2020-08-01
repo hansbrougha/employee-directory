49fc9f62c9fc257dc2b5eac6438a7c0c5af4501e
Andrew Hansbrough <hansbroughku@gmail.com>
2020-08-01 14:13:48 -0500

    Removed Router from App.js since there is only 1 page.

--- src/App.js
+++ src/App.js
@@ -1,22 +1,11 @@
 import React from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { Route } from "react-router-dom";
 import Container from "./components/EmployeeContainer";
 
 function App() {
   return (
     <div>
-      <Router>
-        <Route
-          exact
-          path={process.env.PUBLIC_URL + "/"}
-          component={Container}
-        />
-        <Route
-          exact
-          path={process.env.PUBLIC_URL + "/employee-directory"}
-          component={Container}
-        />
-      </Router>
+      <Route exact path={process.env.PUBLIC_URL + "/"} component={Container} />
     </div>
   );
 }
